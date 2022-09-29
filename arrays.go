809460550ce7d38f947324afcaa8073470885c58
Angel Garcia <engmiguelgarcia@gmail.com>
2022-09-29 15:54:03 -0500

    Wohoo. understanting syntax, filters, arrays on Go language. Great!

---
+++ arrays.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var frutas = []string{"Manzanas", "Uvas", "Peras"}
+	fmt.Println(frutas[1:2] )
+}
---
+++ if.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt")
+
+func main() {
+	puntos := 10
+	if puntos == 10 {
+		fmt.Println("Ok.. Great.. right points.")
+	}
+}
